2e045548de09244c39321910cc333e1ee754fd21
MpaulaH <herrerapaula2005@gmail.com>
2025-11-22 18:51:45 -0500

    feat: componente Perfil dinámico con props

--- Actividad_react_vite/src/componentes/Perfil.jsx
+++ Actividad_react_vite/src/componentes/Perfil.jsx
@@ -1,20 +1,8 @@
-export default function About() {
+export default function About({ resumen }) {
   return (
     <section>
       <h2>Perfil Profesional</h2>
-      <p>
-        Soy una desarrolladora web apasionada por la creación de interfaces modernas y funcionales,
-        con habilidades en React, JavaScript y diseño responsive.
-      </p>
-
-      <h2>Habilidades</h2>
-        <ul>
-          <li>Trabajo en equipo</li>
-          <li>Aprendizaje continuo</li>
-          <li>Comunicación efectiva</li>
-          <li>Resolución de problemas</li>
-          <li>Adecuado uso de herramientas de software</li>
-        </ul>
+      <p>{resumen}</p>
     </section>
-);
+  );
 }
